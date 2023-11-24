816261b2c24ab3536bac112c2c0bbfac215e5625
john <ja617481@gmail.com>
2023-11-24 18:27:02 +0200

    create custom app bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Notes',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+          ),
+          CustomIcon()
+        ],
+      ),
+    );
+  }
+}
+
+
---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white.withOpacity(.2),
+            borderRadius: BorderRadius.circular(18)),
+        child: IconButton(
+          icon: const Icon(
+            Icons.search_rounded,
+            size: 30,
+          ),
+          onPressed: () {},
+        ));
+  }
+}
